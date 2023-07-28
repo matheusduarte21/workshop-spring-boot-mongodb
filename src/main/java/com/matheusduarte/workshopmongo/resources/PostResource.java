7c427aed347e589ed42f8cbb08d316ea25b569ad
Matheus Duarte <122234782+matheusduarte21@users.noreply.github.com>
2023-07-28 19:11:19 -0300

    consulta com vários critérios

--- src/main/java/com/matheusduarte/workshopmongo/resources/PostResource.java
+++ src/main/java/com/matheusduarte/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.matheusduarte.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +33,19 @@ public class PostResource {
 		text = URL.decodeParam(text); // isso decodificar o texto 
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
+	}
 	
+	@GetMapping(value = "/fullsearch") 
+	public ResponseEntity <List<Post>> fullSearch(
+			@RequestParam(value = "text",defaultValue =" ") String text,
+			@RequestParam(value = "minDate",defaultValue =" ") String minDate,
+			@RequestParam(value = "maxDate",defaultValue =" ") String maxDate)
+	{
+		text = URL.decodeParam(text); // isso decodificar o texto
+		Date minD = URL.converDate(minDate, new Date());
+		Date maxD = URL.converDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, minD, maxD);
+		return ResponseEntity.ok().body(list);
 	}
 
 }
