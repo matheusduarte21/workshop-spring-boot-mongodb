0d723127075b8d3e21ed79be5d8e558d8ccd9574
Matheus Duarte <122234782+matheusduarte21@users.noreply.github.com>
2023-07-28 18:15:59 -0300

    Consulta simples com Query Methods

--- src/main/java/com/matheusduarte/workshopmongo/resources/PostResource.java
+++ src/main/java/com/matheusduarte/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.matheusduarte.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheusduarte.workshopmongo.domain.Post;
+import com.matheusduarte.workshopmongo.resources.util.URL;
 import com.matheusduarte.workshopmongo.services.PostService;
 
 @RestController
@@ -23,6 +27,12 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-
+	@GetMapping(value = "/titlesearch") 
+	public ResponseEntity <List<Post>> findByTitle(@RequestParam(value = "text",defaultValue =" ") String text){
+		text = URL.decodeParam(text); // isso decodificar o texto 
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	
+	}
 
 }
--- src/main/java/com/matheusduarte/workshopmongo/services/PostService.java
+++ src/main/java/com/matheusduarte/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.matheusduarte.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class PostService {
 		Optional<Post> userOptional = repo.findById(id);
         Post post = userOptional.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
         return post ;
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
