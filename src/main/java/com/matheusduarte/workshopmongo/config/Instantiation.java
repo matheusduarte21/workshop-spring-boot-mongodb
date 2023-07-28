e3d7b9d6aafea2a2d027953aea90460d70bac6df
Matheus Duarte <122234782+matheusduarte21@users.noreply.github.com>
2023-07-28 14:40:09 -0300

    referenciando os posts dos usuários

--- src/main/java/com/matheusduarte/workshopmongo/config/Instantiation.java
+++ src/main/java/com/matheusduarte/workshopmongo/config/Instantiation.java
@@ -40,8 +40,10 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sfd.parse("21/12/2002"), "Partiu viagem", "vou viajar para SP, abraços!",new AuthorDTO(maria) );
 		Post post2 = new Post(null, sfd.parse("22/12/2002"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-
+  
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post1));
 		
 	}
 	
